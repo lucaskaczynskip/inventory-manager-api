1bfb2be0aa4227cebd34866baad3abdcda7784be
lucaskaczynskip <lucaskaczyn@gmail.com>
2021-12-18 16:00:58 -0300

    routes file and functions

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import "reflect-metadata";
 import express from "express";
 import "./database";
+import { routes } from "./routes";
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 app.listen(port, () => console.log("Server is running!"));
