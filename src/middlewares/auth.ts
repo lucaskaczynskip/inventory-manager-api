c6e712ee281da2aeff1d5a1055bf5461e387b1fd
lucaskaczynskip <lucaskaczyn@gmail.com>
2021-12-21 20:49:55 -0300

    feat: auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+import jwt from "../database/secret";
+
+export default function (req: Request, res: Response, next: NextFunction) {
+  try {
+    const auth = req.headers.authorization;
+
+    if (!auth) {
+      return res.status(400).json({
+        error: true,
+        message: "JWT is missing."
+      });
+    }
+
+    const [, token] = auth.split(" ");
+
+    const decoded = verify(token, jwt.secret);
+
+    if (!decoded) {
+      return res.status(400).json({
+        error: true,
+        message: "JWT invalid."
+      });
+    }
+
+    return next();
+
+  } catch(err) {
+    return res.status(400).json({
+      error: true, 
+      message: err.message
+    });
+  }
+}
