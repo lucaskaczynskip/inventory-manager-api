eaf224fe73c22d51b95ede0980e1dc01c63812c0
lucaskaczynskip <lucaskaczyn@gmail.com>
2021-12-21 22:06:33 -0300

    feat: get user by id controller

---
+++ src/controllers/GetUserByIdController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { GetUserByIdService } from "../services/GetUserByIdService";
+
+export class GetUserByIdController {
+  async handle(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const service = new GetUserByIdService();
+    const user = await service.execute(id);
+
+    if (user instanceof Error) {
+      return res.status(400).json({
+        error: true,
+        message: user.message
+      });
+    }
+
+    return res.status(200).json({
+      error: false,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        created_at: user.created_at
+      }
+    });
+  }
+}
