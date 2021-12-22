f4757de7faaf9574d17d15853c319da1b1c754e6
lucaskaczynskip <lucaskaczyn@gmail.com>
2021-12-21 21:42:43 -0300

    feat: get users controller

---
+++ src/controllers/GetAllUsersService.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { GetAllUsersService } from "../services/GetAllUsersService";
+
+export class GetAllUserController {
+  async handle(req: Request, res: Response) {
+    const service = new GetAllUsersService();
+    
+    const result = await service.execute();
+    const users = result.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        created_at: user.created_at
+      }
+    });
+
+    return res.status(400).json({
+      error: false,
+      users: users 
+    });
+  }
+}
