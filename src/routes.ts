db2008253131b93f3fe64cf198771b7bffc2230e
lucaskaczynskip <lucaskaczyn@gmail.com>
2021-12-21 22:06:45 -0300

    feat: get user by id route

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 // Controllers
 import { GetAllUserController } from "./controllers/GetAllUsersService";
+import { GetUserByIdController } from "./controllers/GetUserByIdController";
 import { UserCreateController } from "./controllers/UserCreateController";
 import { UserLoginController } from "./controllers/UserLoginController";
 
@@ -14,5 +15,6 @@ routes.post("/login", new UserLoginController().handle);
 
 routes.post("/users", new UserCreateController().create);
 routes.get("/users", auth, new GetAllUserController().handle);
+routes.get("/users/:id", auth, new GetUserByIdController().handle);
 
 export { routes };
