c0cadd6132d3ae26de07f78dae8811c6aa1ec7a6
lucaskaczynskip <lucaskaczyn@gmail.com>
2022-01-07 15:28:59 -0300

    refactoring

--- src/controllers/GetAllUsersService.ts
+++ src/controllers/GetAllUsersService.ts
@@ -5,7 +5,20 @@ export class GetAllUserController {
   async handle(req: Request, res: Response) {
     const service = new GetAllUsersService();
     
-    const users = await service.execute();
+    const usersList = await service.execute();
+
+    const users = usersList.map(user => {
+      let i = 0;
+      
+      user.products.forEach(u => i+= u.quantity);
+
+      return {
+        name: user.name,
+        email: user.email,
+        created_at: user.created_at,
+        products: `The user has ${i} products in inventory` 
+      }
+    });
 
     return res.status(200).json({
       users: users 
